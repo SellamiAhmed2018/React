2cd2d5b5f297d4c9dc0b8390c486649fd1b6cdce
SellamiAhmed2018 <66868625+SellamiAhmed2018@users.noreply.github.com>
2020-06-20 14:34:07 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import "./style.css";
+import "bootstrap/dist/css/bootstrap.css";
+
+import image from "./imageInSrc.jpg";
+import video from "./Wildlife.wmv";
+
+class App extends Component {
+	render() {
+		return (
+			<>
+				<div style={{ border: "solid 1px black", maxWidth: "100vw" }}>
+					<h1 className={"title red"}>Your name here</h1>
+					<br />
+					<img src={image} />
+					<br />
+					<img src={"/imageInPublic.jpg"} />
+				</div>
+				<div>
+					<video width={"320"} height={"240"} controls src={video} type={"video/mp4"}>
+					</video>
+				</div>
+			</>
+
+		);
+	}
+}
+
+export default App;
